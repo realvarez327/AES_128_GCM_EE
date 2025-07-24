90b8f9f4cd1f027852fe0a8f83cde7ba0771a15f
NoThanks36927 <rebecca.alvarez@syk.fi>
2025-07-24 17:40:01 +0300

    Implemented incrementation step, likely not best possible but exists. #todo, need to write test case

--- src/main/java/ee/GCM.java
+++ src/main/java/ee/GCM.java
@@ -20,9 +20,49 @@ public class GCM {
     }
 
 
+    static int[] getNAndU(String P){
+        //calculate length of P in bits
+        int[] nAndU = new int[2];
+        int stringBitLength = P.length()*16;
+        if(stringBitLength%128==0){
+            nAndU[0]=(stringBitLength)/128;
+            nAndU[1]=128;
+            return nAndU;
+        }
+        //only reach here if not a nicely blocked thing
+        stringBitLength -= 128;
+        nAndU[0] = stringBitLength/128;
+        nAndU[1]= stringBitLength%128;
+        return nAndU;
+    }
+
+    static BitSet incr(BitSet input){
+        //input is 16 bytes
+        //lord forgive me for this awful code i am going to write
+        int size = input.size();
+        BitSet toReturn = input;
+        if(toReturn.get(size-1-5)){
+            //check if "32" bit is set
+            toReturn.set(size-1-5, size-1, false);
+        }else{
+            //increment last integer spot
+            String s = toReturn.toString().substring(size-1-5, size-1);
+            for (int i = 0; i < s.length(); i++) {
+                if(s.charAt(i)=='0'){
+                    toReturn.set(size-6+i, 0);
+                }else{
+                    toReturn.set(size-6+i, 1);
+                }
+            }
+        }
+        return toReturn;
+    }
+
 
     static void gcmEncryption(String K, String P, String IV, String AAD){
-        int n = 2; //todo this should eventually have amount of full "blocks in P" IMPLEMENT
+        int[] nAndU = getNAndU(P);
+        int n = nAndU[0];
+        int m = nAndU[1]; // try to just use the int array actually
         int[] H = AES_128.cipher(K,new int[][]{
                 {0,0,0,0},
                 {0,0,0,0},
@@ -31,6 +71,10 @@ public class GCM {
         });
         BitSet[] Y = new BitSet[n+1];
         Y[0] = stringToBitset(IV + 0b00000000000000000000000000000001);
+        for (int i = 1; i <= n; i++) {
+            //increment it
+            Y[i] = incr(Y[i-1]);
+        }
 
     }
 }
